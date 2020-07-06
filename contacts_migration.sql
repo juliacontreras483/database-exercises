9a1b61267e65f79cb5a3e30dcf4fac4ce36fdcb5
Julia Contreras <juliacontreras483@gmail.com>
2020-07-06 15:54:21 -0500

    bonus

---
+++ contacts_migration.sql
@@ -0,0 +1,13 @@
+-- Create a contacts migration file.
+-- Use codeup_test_db.
+-- Create a table called contacts that incorporates the following:
+-- * Id - Integer
+-- * Name - String
+-- * Email - String
+-- * Phone Number - String
+-- * Created At - Datetime
+-- * Updated At - Datetime
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS contacts;
