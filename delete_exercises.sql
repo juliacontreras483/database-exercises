779b6b0f2e449dbf5302712857333c132431458a
Julia Contreras <juliacontreras483@gmail.com>
2020-07-08 11:15:23 -0500

    inserting notes examples fixing previous exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -12,3 +12,42 @@ delete from albums where genre = 'disco';
 -- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
 -- select * from albums where artist = 'James Horner';
 delete from albums where artist = 'James Horner';
+
+
+
+#
+# VIVIAN CODE BELOW REVIEW COMPARE AND REFERENCE
+# USE codeup_test_db;
+#
+# # Write SELECT statements for:
+# -- Albums released after 1991
+# SELECT * FROM albums
+# WHERE release_date > '1991';
+#
+#
+# -- Albums with the genre 'disco'
+# SELECT * FROM albums
+# WHERE genre LIKE '%disco%';
+#
+#
+# -- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+# SELECT * FROM albums
+# WHERE artist LIKE '%whitney houston%';
+#
+#
+# # DELETEs
+#
+# DELETE FROM albums
+# WHERE release_date > '1991';
+#
+#
+#
+# DELETE FROM albums
+# WHERE genre LIKE '%disco%';
+#
+#
+#
+#
+# DELETE FROM albums
+# WHERE artist LIKE '%whitney houston%';
+# SELECT * FROM albums;
--- select_exercises.sql
+++ select_exercises.sql
@@ -7,14 +7,63 @@ select * from albums where artist = 'Pink Floyd';
 select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT genre FROM albums
-WHERE `name` = "Nevermind";
+WHERE name = "Nevermind";
 
-SELECT `name` FROM albums
+SELECT name FROM albums
 WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT `name` FROM albums
+SELECT name FROM albums
 WHERE sales < 20;
 
-SELECT `name`, genre
+SELECT name, genre
 FROM albums
 WHERE genre = 'Rock';
+
+# VIVIAN EXAMPLE COMPARE CODE
+# USE codeup_test_db;
+#
+# -- The name of all albums by Pink Floyd.
+# SELECT 'The name of all albums by Pink Floyd' AS 'Exercise 1';
+#
+# SELECT name FROM albums
+# WHERE artist = 'Pink Floyd';
+#
+#
+# -- The year Sgt. Pepper's Lonely Hearts Club Band was released
+# SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Exercise 2';
+#
+# SELECT release_date FROM albums
+# WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+#
+#
+# -- The genre for Nevermind
+#
+# SELECT 'The genre for Nevermind' AS 'Exercise 3';
+#
+# SELECT genre FROM albums
+# WHERE name = "Nevermind";
+#
+#
+# -- Which albums were released in the 1990s
+#
+# SELECT 'Which albums were released in the 1990s' AS 'Exercise 4';
+#
+# SELECT name FROM albums
+# WHERE release_date BETWEEN 1990 AND 1999;
+#
+#
+# -- Which albums had less than 20 million certified sales
+#
+# SELECT 'Which albums had less than 20 million certified sales' AS 'Exercise 5';
+# SELECT name FROM albums
+# WHERE sales < 20;
+#
+#
+#
+#
+#
+# -- All the albums in the rock genre. Is this all the rock albums in the table?
+#
+# SELECT 'All the albums in the rock genre. Is this all the rock albums in the table?' AS 'Exercise 6';
+# SELECT name, genre FROM albums
+# WHERE genre LIKE '%rock%';
