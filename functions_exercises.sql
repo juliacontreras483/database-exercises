638911a57a1cdabb962dc728c0ea886e5f5897a0
Julia Contreras <juliacontreras483@gmail.com>
2020-07-09 10:17:26 -0500

    starting group by & aliases exer.

---
+++ functions_exercises.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+# Problem 2
+# Update your queries for employees whose names start and end with 'E'.
+# Use concat() to combine their first and last name together as a single column in your results.
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+
+# Problem 3
+
+# For your query of employees born on Christmas and hired in the 90s,
+# use datediff() to find how many days they have been working at the company
+# (Hint: You will also need to use now() or curdate())
+
+SELECT *, CONCAT('Days at company: ', DATEDIFF(NOW(), hire_date))
+FROM employees
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC;
